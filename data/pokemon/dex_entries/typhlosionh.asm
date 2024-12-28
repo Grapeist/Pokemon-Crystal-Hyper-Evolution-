fada4ef20355a733a58506e0ade88a6e62923519
Grapeist <58837528+Grapeist@users.noreply.github.com>
2024-12-28 08:08:15 -0800

    Add files via upload

---
+++ data/pokemon/dex_entries/typhlosionh.asm
@@ -0,0 +1,10 @@
+	db "GHOST FLAME@" ; species name
+	dw 503, 1539 ; height, weight
+
+	db   ".....ERROR....."
+	next "Base speciese is"
+	next "TYPHLOSION, from"
+
+	page "anceint SINNOH,"
+	next "or better known"
+	next "as HISUI.@"
